02af4e99f88076eb2914d0a4bf8912fc5e65a0ef
Andy Bulka <abulka@gmail.com>
2017-03-03 22:58:51 +1100

    added command line parsing with options --noadvice --noreport

--- pipfix.js
+++ pipfix.js
@@ -1,19 +1,29 @@
 'use strict';
 
-const format = require('pretty-format')  // https://github.com/facebook/jest/tree/master/packages/pretty-format
 // const format = require('pretty-format')  // https://github.com/facebook/jest/tree/master/packages/pretty-format
-
-let {Python, Pip, Brain} = require('./lib.js')
+const format = require('pretty-format')  // https://github.com/facebook/jest/tree/master/packages/pretty-format
+const prog = require('caporal');  // https://github.com/mattallty/Caporal.js
+let {Brain} = require('./lib.js')
 let {advice} = require('./advice.js')
 
-// TODO should use proper command line parsing and have options etc.
+prog
+  .version('1.0.0')
+  .description('Reports python installed and any related pips')
+  .option('--noadvice', 'Give some advice', prog.BOOL)
+  .option('--noreport', 'No neport', prog.BOOL)
+  .action(function(args, options, logger) {
+    logger.info("pipfix is analysing...", options)
 
-let brain = new Brain()
+    let brain = new Brain()
 
-for (let el of [...brain.pythons, ...brain.pips])
-  console.log(`${el.path} ---------- ${format(el.report_obj)}`)
+    if (! options.noreport)
+      for (let el of [...brain.pythons, ...brain.pips])
+        console.log(`${el.path} ---------- ${format(el.report_obj)}`)
 
-advice(brain)
+    if (! options.noadvice)
+      advice(brain)
 
-console.log('DONE ')
+    console.log('DONE ')
+  });
 
+prog.parse(process.argv);
